7befa95f315b2b49978ceaa0e01f0fa64d28bd22
Dennis van Beelen <dvbeelen@gmail.com>
2020-11-17 14:14:25 +0100

    routing added

--- Front-end/AirQualityDashboard/src/app/app-routing.module.ts
+++ Front-end/AirQualityDashboard/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {InformationComponent} from './components/information/information.component';
+import {StatisticsComponent} from './components/statistics/statistics.component';
+import {UnitsComponent} from './components/units/units.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'information', component: InformationComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'units', component: UnitsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-end/AirQualityDashboard/src/app/app.module.ts
+++ Front-end/AirQualityDashboard/src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UnitsComponent } from './components/units/units.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { InformationComponent } from './components/information/information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    DashboardComponent,
+    UnitsComponent,
+    StatisticsComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: []
 })
-export class AppModule { }
+export class AppModule {
+}
