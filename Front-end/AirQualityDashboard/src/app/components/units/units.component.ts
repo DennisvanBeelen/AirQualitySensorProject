73da16d55fea2edfc0cee5204f931ff558904f60
Dennis van Beelen <dvbeelen@gmail.com>
2020-12-14 17:12:25 +0100

    A all sensor data table in the popup + made it work with the actual data.

--- Front-end/AirQualityDashboard/src/app/components/units/units.component.ts
+++ Front-end/AirQualityDashboard/src/app/components/units/units.component.ts
@@ -4,11 +4,12 @@ import {MatTableDataSource} from "@angular/material/table";
 import {MatDialog} from "@angular/material/dialog";
 import {UnitsPopupComponent} from "./units-popup/units-popup.component";
 import {SensorType, SensorUnits} from "../../interfaces";
-import {firebaseService} from "../../firebaseService";
+import {firebaseService} from "../../services/firebaseService";
+import {dataService} from "../../services/dataService";
 
 @Component({
   selector: 'app-units',
-  providers: [firebaseService],
+  providers: [firebaseService, dataService],
   templateUrl: './units.component.html',
   styleUrls: ['./units.component.scss'],
   animations: [
@@ -20,20 +21,32 @@ import {firebaseService} from "../../firebaseService";
 })
 export class UnitsComponent implements OnInit {
 
+  @Input() compactMode = false; // compact mode for dashboard.
+
   dataSource;
   columnsToDisplay = ['location', 'id', 'timestamp', 'sensorData'];
   expandedElement: SensorUnits | null;
   extraAllowance = 0.1; // amount you can go over sensorMin and sensorMax before getting a sad smiley.
-  @Input() compactMode = false; // compact mode for dashboard.
-  sensorDataCollectioPath = 'sensorDataTest';
+  sensorDataCollectioPath = 'sensorData';
 
-  constructor(public dialog: MatDialog, private firebaseServ: firebaseService) {
+  constructor(public dialog: MatDialog, private firebaseServ: firebaseService, public dataServ: dataService) {
   }
 
   ngOnInit(): void {
     this.checkForCompactMode();
+    this.getLiveDataFromDB();
+  }
+
+  checkForCompactMode() {
+    if (this.compactMode) {
+      this.columnsToDisplay = ['location', 'id', 'timestamp'];
+    }
+  }
+
+  getLiveDataFromDB() {
     this.firebaseServ.getLiveCollectionFromFirebase(this.sensorDataCollectioPath).subscribe(data => (this.dataSource = new MatTableDataSource(data)));
   }
+
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -47,124 +60,13 @@ export class UnitsComponent implements OnInit {
 
   addSafetyDataToDialogData(sensorRowData) {
     sensorRowData = sensorRowData.payload.doc.data();
-    sensorRowData.safetyRating = [];
-    sensorRowData.safetyColor = [];
-
-    for (let sensor of sensorRowData.sensorData) {
-      let sr = this.getSafetyRating(sensor.sensorName, sensor.sensorValue);
-      sensorRowData.safetyRating.push(sr);
-      sensorRowData.safetyColor.push(this.getSafetyColorCode(sr))
+    for (let sensorData of this.dataServ.createArrayFromObject(sensorRowData.sensorData)){
+      for (let sensor of sensorData.data) {
+        let sr = this.dataServ.getSafetyRating(sensor.sensorType, sensor.sensorValue);
+        sensor.safetyRating = sr;
+        sensor.safetyColor = this.dataServ.getSafetyColorCode(sr);
+      }
     }
     return sensorRowData;
   }
-
-  getSafetyColorCode(safetyRating: string) {
-    switch (safetyRating) {
-      case "mood":
-        return "#03EA18"
-
-      case "sentiment_dissatisfied":
-        return "#fbbe00"
-
-      case "mood_bad":
-        return "#ff0000"
-    }
-  }
-
-  getSafetyRating(sensorName, sensorValue) {
-    let sensor = this.getCorrectSensorType(sensorName);
-
-    if (sensorValue < sensor.maximumValue && sensorValue > sensor.minimalValue) {
-      return "mood"; // mood is happy smiley
-    } else if (sensorValue < (sensor.maximumValue + (sensor.maximumValue * this.extraAllowance)) && sensorValue > (sensor.minimalValue - (sensor.minimalValue * this.extraAllowance))) {
-      return "sentiment_dissatisfied"; // sentiment_dissatisfied is not super happy smiley
-    } else {
-      return "mood_bad"; // sad smiley
-    }
-  }
-
-  getCorrectSensorType(sensorName) {
-    switch (sensorName) {
-      case SensorType.BAROMETER.name:
-        return SensorType.BAROMETER;
-
-      case SensorType.ALTIMETER.name:
-        return SensorType.ALTIMETER;
-
-      case SensorType.TEMPERATURE.name:
-        return SensorType.TEMPERATURE;
-
-      case SensorType.HUMIDITY.name:
-        return SensorType.HUMIDITY;
-
-      case SensorType.AIRQUALITY.name:
-        return SensorType.AIRQUALITY;
-    }
-  }
-
-  checkForCompactMode() {
-    if (this.compactMode) {
-      this.columnsToDisplay = ['location', 'id', 'timestamp'];
-    }
-  }
-
-  createReadableTimestamp(timestamp) {
-    return new Date(timestamp.seconds * 1000).toLocaleString();
-  }
-
-  // putTestDataInDB() {
-  //   for (let i = 0; i < ELEMENT_DATA.length; i++) {
-  //     this.firebaseServ.setDataInFirebase('sensorDataTest', ELEMENT_DATA[i].id + ELEMENT_DATA[i].location, ELEMENT_DATA[i])
-  //   }
-  // }
-
 }
-
-// mockdata untill we get a working DB.
-// let ELEMENT_DATA: SensorUnits[] = [
-//   {
-//     id: 123456789,
-//     location: "Livingroom",
-//     timestamp: new Date(),
-//     sensorData: [
-//       {sensorName: SensorType.BAROMETER.name, sensorValue: 1.2, sensorDataUnit: SensorType.BAROMETER.unitType},
-//       {sensorName: SensorType.ALTIMETER.name, sensorValue: 2, sensorDataUnit: SensorType.ALTIMETER.unitType},
-//       {sensorName: SensorType.TEMPERATURE.name, sensorValue: 80, sensorDataUnit: SensorType.TEMPERATURE.unitType},
-//       {sensorName: SensorType.HUMIDITY.name, sensorValue: 80, sensorDataUnit: SensorType.HUMIDITY.unitType},
-//       {sensorName: SensorType.AIRQUALITY.name, sensorValue: 35, sensorDataUnit: SensorType.AIRQUALITY.unitType}]
-//   },
-//   {
-//     id: 123876789,
-//     location: "Bedroom",
-//     timestamp: new Date(),
-//     sensorData: [
-//       {sensorName: SensorType.BAROMETER.name, sensorValue: 1.2, sensorDataUnit: SensorType.BAROMETER.unitType},
-//       {sensorName: SensorType.ALTIMETER.name, sensorValue: 2, sensorDataUnit: SensorType.ALTIMETER.unitType},
-//       {sensorName: SensorType.TEMPERATURE.name, sensorValue: 80, sensorDataUnit: SensorType.TEMPERATURE.unitType},
-//       {sensorName: SensorType.HUMIDITY.name, sensorValue: 80, sensorDataUnit: SensorType.HUMIDITY.unitType},
-//       {sensorName: SensorType.AIRQUALITY.name, sensorValue: 35, sensorDataUnit: SensorType.AIRQUALITY.unitType}]
-//   }, {
-//     id: 137656789,
-//     location: "Attic",
-//     timestamp: new Date(),
-//     sensorData: [
-//       {sensorName: SensorType.BAROMETER.name, sensorValue: 1.2, sensorDataUnit: SensorType.BAROMETER.unitType},
-//       {sensorName: SensorType.ALTIMETER.name, sensorValue: 2, sensorDataUnit: SensorType.ALTIMETER.unitType},
-//       {sensorName: SensorType.TEMPERATURE.name, sensorValue: 80, sensorDataUnit: SensorType.TEMPERATURE.unitType},
-//       {sensorName: SensorType.HUMIDITY.name, sensorValue: 80, sensorDataUnit: SensorType.HUMIDITY.unitType},
-//       {sensorName: SensorType.AIRQUALITY.name, sensorValue: 35, sensorDataUnit: SensorType.AIRQUALITY.unitType}]
-//   }, {
-//     id: 912987489,
-//     location: "Office",
-//     sensorData: [
-//       {
-//         timestamp: new Date(),
-//         data: [{sensorName: SensorType.BAROMETER.name, sensorValue: 1.2, sensorDataUnit: SensorType.BAROMETER.unitType},
-//           {sensorName: SensorType.ALTIMETER.name, sensorValue: 2, sensorDataUnit: SensorType.ALTIMETER.unitType},
-//           {sensorName: SensorType.TEMPERATURE.name, sensorValue: 80, sensorDataUnit: SensorType.TEMPERATURE.unitType},
-//           {sensorName: SensorType.HUMIDITY.name, sensorValue: 80, sensorDataUnit: SensorType.HUMIDITY.unitType},
-//           {sensorName: SensorType.AIRQUALITY.name, sensorValue: 35, sensorDataUnit: SensorType.AIRQUALITY.unitType}]
-//       }
-//     ]
-//   }
-// ];
---
+++ Front-end/AirQualityDashboard/src/app/services/dataService.ts
@@ -0,0 +1,84 @@
+import {Injectable} from "@angular/core";
+import {SensorType} from "../interfaces";
+
+@Injectable()
+export class dataService {
+  extraAllowance = 0.1;
+
+  getMostRecentSensorReading(element) {
+    let array = this.createArrayFromObject(element);
+    let locationOfMostRecentTimestamp = null;
+    for (let i = 0; i < array.length; i++) {
+      if (locationOfMostRecentTimestamp === null) {
+        locationOfMostRecentTimestamp = i;
+      } else {
+        if (array[i].timestamp > array[locationOfMostRecentTimestamp].timestamp) {
+          locationOfMostRecentTimestamp = i;
+        }
+      }
+    }
+    return array[locationOfMostRecentTimestamp];
+  }
+
+  createArrayFromObject(object) {
+    return Object.keys(object).map(function (objectIndex) {
+      return object[objectIndex];
+    });
+  }
+
+  createReadableTimestamp(timestamp) {
+    return new Date(timestamp.seconds * 1000).toLocaleString();
+  }
+
+  getSafetyColorCode(safetyRating: string) {
+    switch (safetyRating) {
+      case "mood":
+        return "#03EA18"
+
+      case "sentiment_dissatisfied":
+        return "#fbbe00"
+
+      case "mood_bad":
+        return "#ff0000"
+    }
+  }
+
+  getSafetyRating(sensorName, sensorValue) {
+    let sensor = this.getCorrectSensorType(sensorName);
+
+    if (sensorValue < sensor.maximumValue && sensorValue > sensor.minimalValue) {
+      return "mood"; // mood is happy smiley
+    } else if (sensorValue < (sensor.maximumValue + (sensor.maximumValue * this.extraAllowance)) && sensorValue > (sensor.minimalValue - (sensor.minimalValue * this.extraAllowance))) {
+      return "sentiment_dissatisfied"; // sentiment_dissatisfied is not super happy smiley
+    } else {
+      return "mood_bad"; // sad smiley
+    }
+  }
+
+  getCorrectSensorType(sensorName) {
+    switch (sensorName.toLowerCase()) {
+      case SensorType.BAROMETER.type.toLowerCase():
+        return SensorType.BAROMETER;
+
+      case SensorType.ALTITUDE.type.toLowerCase():
+        return SensorType.ALTITUDE;
+
+      case SensorType.TEMPERATURE.type.toLowerCase():
+        return SensorType.TEMPERATURE;
+
+      case SensorType.HUMIDITY.type.toLowerCase():
+        return SensorType.HUMIDITY;
+
+      case SensorType.AIRQUALITY.type.toLowerCase():
+        return SensorType.AIRQUALITY;
+
+      case SensorType.CO2.type.toLowerCase():
+        return SensorType.CO2;
+    }
+  }
+
+  roundSensorData(sensorValue) {
+    return Math.round((Number(sensorValue) + Number.EPSILON) * 100) / 100
+  }
+}
+
